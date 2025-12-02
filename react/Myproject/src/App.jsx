b0ff4447fe228dab8f66de114380adc3e89591fc
Sai Krishnan S <saikrishnan.s2024cse@sece.ac.in>
2025-12-02 12:27:12 +0530

    feat: initialize React project with Vite, create registration form and styling
    - Added package.json with dependencies for React, Vite, and ESLint.
    - Included Vite logo SVG in public directory.
    - Created App.css for styling the registration form.
    - Developed App.jsx component for the registration form layout.
    - Added React logo SVG in assets directory.
    - Set up main.jsx for rendering the App component.
    - Configured Vite with vite.config.js.
    - Created basic HTML tables in table.html and table-2.html for demonstration.

---
+++ react/Myproject/src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css'
+function App() {
+  return (
+    <>
+    <h1>Registration Form</h1>
+    <div className="container">
+      <form action="">
+        <label htmlFor="fname">First Name:</label>
+        <input type="text" id="fname" name="fname" placeholder="Your first name.." />
+        <label htmlFor="lname">Last Name:</label>
+        <input type="text" id="lname" name="lname" placeholder="Your last name.." />
+        <label htmlFor="email">Email:</label>
+        <input type="text" id="email" name="email" placeholder="Your email.." />
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" name="password" placeholder="Your password.." />
+        <label htmlFor="cpassword">Confirm Password:</label>
+        <input type="password" id="cpassword" name="cpassword" placeholder="Your password.." />
+        <input type="submit" value="Submit" />
+        <input type="reset" value="Reset" />
+      </form>
+    </div>
+    <p>Already have an account? <a href="#">Login</a></p>
+    <p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
+    </>
+  )
+}
+
+export default App
